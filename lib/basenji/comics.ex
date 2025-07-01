76dd7e080a872708f05ef698725a10c55c29b2d7
Cam Cook <cam.cook.codes@gmail.com>
2025-07-01 12:23:28 -0400

    feat: crud comics

--- lib/basenji/comics.ex
+++ lib/basenji/comics.ex
@@ -6,12 +6,56 @@ defmodule Basenji.Comics do
   alias Basenji.Comic
   alias Basenji.Repo
 
+  def create_comic(attrs, opts \\ []) do
+    opts = Keyword.merge([repo_opts: []], opts)
+
+    %Comic{}
+    |> Comic.changeset(attrs)
+    |> Repo.insert(opts[:repo_opts])
+  end
+
   def list_comics(opts \\ []) do
     opts = Keyword.merge([repo_opts: []], opts)
 
     Comic
     |> reduce_opts(opts)
     |> Repo.all(opts[:repo_opts])
+  end
+
+  def get_comic(id, opts \\ []) do
+    opts = Keyword.merge([repo_opts: []], opts)
+
+    from(c in Comic, where: c.id == ^id)
+    |> reduce_opts(opts)
+    |> reduce_comic_opts(opts)
+    |> Repo.one(opts[:repo_opts])
+  end
+
+  def update_comic(%Comic{} = comic, attrs) do
+    comic
+    |> Comic.changeset(attrs)
+    |> Repo.update()
+  end
+
+  def delete_comic(nil), do: nil
+
+  def delete_comic(comic_id) when is_binary(comic_id) do
+    %Comic{id: comic_id}
+    |> delete_comic()
+  end
+
+  def delete_comic(%Comic{id: _comic_id} = comic) do
+    Repo.delete(comic)
+  end
+
+  defp reduce_comic_opts(query, opts) do
+    Enum.reduce(opts, query, fn
+      {_any, ""}, query ->
+        query
+
+      _, query ->
+        query
+    end)
   end
 
   defp reduce_opts(query, opts) do
--- test/basenji/comics_test.exs
+++ test/basenji/comics_test.exs
@@ -3,20 +3,83 @@ defmodule Basenji.ComicsTest do
 
   alias Basenji.Comics
 
-  test "list_comics/1" do
-    expected_count = 100
-    asserted = insert_list(expected_count, :comic)
-    retrieved = Comics.list_comics()
-    assert Enum.count(retrieved) == expected_count
+  describe "crud" do
+    test "create_comic/2" do
+      to_assert = build_list(100, :comic)
+      refute Enum.empty?(to_assert)
 
-    Enum.each(asserted, fn comic ->
-      assert [^comic] = Enum.filter(retrieved, &(&1.id == comic.id))
-      assert comic.title
-      assert comic.description
-      assert comic.author
-      assert comic.resource_location
-      assert comic.released_year > 0
-      assert comic.page_count > 0
-    end)
+      Enum.each(to_assert, fn comic ->
+        {:ok, created} =
+          comic
+          |> Map.from_struct()
+          |> Comics.create_comic()
+
+        assert created.id
+        assert created.title == comic.title
+      end)
+    end
+
+    test "list_comics/1" do
+      expected_count = 100
+      asserted = insert_list(expected_count, :comic)
+      retrieved = Comics.list_comics()
+      assert Enum.count(retrieved) == expected_count
+
+      Enum.each(asserted, fn comic ->
+        assert [^comic] = Enum.filter(retrieved, &(&1.id == comic.id))
+        assert comic.title
+        assert comic.description
+        assert comic.author
+        assert comic.resource_location
+        assert comic.released_year > 0
+        assert comic.page_count > 0
+      end)
+    end
+
+    test "get_comic/2" do
+      asserted = insert_list(100, :comic)
+      refute Enum.empty?(asserted)
+
+      Enum.each(asserted, fn comic ->
+        assert comic == Comics.get_comic(comic.id)
+      end)
+    end
+
+    test "update_comic/2" do
+      asserted = insert_list(100, :comic)
+      refute Enum.empty?(asserted)
+
+      Enum.each(asserted, fn comic ->
+        updated_attrs = %{
+          title: Faker.Lorem.sentence(),
+          author: Faker.Person.name(),
+          description: Faker.Lorem.paragraph(2),
+          resource_location: Faker.App.name() <> ".cbz",
+          released_year: Enum.random(2015..2025),
+          page_count: Enum.random(1..1000)
+        }
+
+        {:ok, updated} = Comics.update_comic(comic, updated_attrs)
+        assert updated.title == updated_attrs.title
+        assert updated.author == updated_attrs.author
+        assert updated.description == updated_attrs.description
+        assert updated.resource_location == updated_attrs.resource_location
+        assert updated.released_year == updated_attrs.released_year
+        assert updated.page_count == updated_attrs.page_count
+      end)
+    end
+
+    test "delete_comic/2" do
+      to_delete = insert_list(100, :comic)
+      refute Enum.empty?(to_delete)
+
+      Enum.each(to_delete, fn comic ->
+        # Either by ID or by struct
+        ref = Enum.random([comic, comic.id])
+        {:ok, deleted} = Comics.delete_comic(ref)
+        assert comic.id == deleted.id
+        refute Comics.get_comic(deleted.id)
+      end)
+    end
   end
 end
