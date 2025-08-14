ce1f772aa34b2f3b166dd0e73b5dfafdb2cdccd0
Cam Cook <cam.cook.codes@gmail.com>
2025-08-14 17:05:50 -0400

    feat(accounts): next pass

--- lib/basenji_web/live/user_live/login.ex
+++ lib/basenji_web/live/user_live/login.ex
@@ -6,7 +6,6 @@ defmodule BasenjiWeb.UserLive.Login do
   @impl true
   def render(assigns) do
     ~H"""
-    <Layouts.app flash={@flash} current_scope={@current_scope}>
       <div class="mx-auto max-w-sm space-y-4">
         <div class="text-center">
           <.header>
@@ -88,7 +87,6 @@ defmodule BasenjiWeb.UserLive.Login do
           </.button>
         </.form>
       </div>
-    </Layouts.app>
     """
   end
 
--- lib/basenji_web/live/user_live/registration.ex
+++ lib/basenji_web/live/user_live/registration.ex
@@ -7,7 +7,6 @@ defmodule BasenjiWeb.UserLive.Registration do
   @impl true
   def render(assigns) do
     ~H"""
-    <Layouts.app flash={@flash} current_scope={@current_scope}>
       <div class="mx-auto max-w-sm">
         <div class="text-center">
           <.header>
@@ -37,7 +36,6 @@ defmodule BasenjiWeb.UserLive.Registration do
           </.button>
         </.form>
       </div>
-    </Layouts.app>
     """
   end
 
